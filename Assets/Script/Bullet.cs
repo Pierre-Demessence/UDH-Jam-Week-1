21c656bab19438f1c7f2acc2af46456fc5302974
Pierre Demessence <pierre.demessence@gmail.com>
2018-10-06 15:03:56 +0200

    First things

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _speed = 6;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.velocity = new Vector2(_speed, 0);
+    }
+}
---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Bullet _bullet;
+    [SerializeField] private float _rateOfFire = 1;
+    private float _lastHitTime;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && ((Time.time - _lastHitTime) > _rateOfFire))
+        {
+            Instantiate(_bullet, transform);
+            _lastHitTime = Time.time;
+        }
+    }
+}
