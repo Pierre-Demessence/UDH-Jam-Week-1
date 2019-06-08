626a680a9aca8e000a6de057bffeff835f0bb421
Pierre Demessence <pierre.demessence@gmail.com>
2019-06-08 20:43:31 +0200

    Added blink animation and invincibility blink

---
+++ Assets/Script/Blink.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Blink : MonoBehaviour
+{
+    [SerializeField] private float _animationDelay;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private void ChangeAlpha(float alpha)
+    {
+        var c = _spriteRenderer.color;
+        c.a = alpha;
+        _spriteRenderer.color = c;
+    }
+
+    private IEnumerator BlinkCoroutine(float duration)
+    {
+        var endTime = Time.time + duration;
+
+        do
+        {
+            ChangeAlpha(1 - Mathf.PingPong((endTime - Time.time) / _animationDelay, 1));
+            yield return new WaitForEndOfFrame();
+        } while (Time.time <= endTime);
+
+        ChangeAlpha(1);
+    }
+
+    public void DoBlink(float duration)
+    {
+        StartCoroutine(BlinkCoroutine(duration));
+    }
+}
--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
     [SerializeField] private float _health = 10;
     [SerializeField] private float _invulnerabilityTime;
     private float _lastDamageTime;
+    [SerializeField] private InvincibilityTriggeredEvent _onInvincibilityTriggered;
     [SerializeField] private TakeDamageEvent _onTakeDamage;
 
     public float Value => _health;
@@ -18,7 +19,11 @@ public class Health : MonoBehaviour
         _health -= amount;
         if (_health <= 0) Destroy(gameObject);
         _onTakeDamage.Invoke(_health);
-        _lastDamageTime = Time.time;
+        if (_invulnerabilityTime > 0)
+        {
+            _lastDamageTime = Time.time;
+            _onInvincibilityTriggered.Invoke(_invulnerabilityTime);
+        }
     }
 
     public void Heal(float amount)
@@ -29,5 +34,10 @@ public class Health : MonoBehaviour
 
 [Serializable]
 internal class TakeDamageEvent : UnityEvent<float>
+{
+}
+
+[Serializable]
+internal class InvincibilityTriggeredEvent : UnityEvent<float>
 {
 }
