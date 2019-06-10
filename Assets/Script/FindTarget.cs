1b21dd5b9642b9a6a40a8944e900ffadde7c74d9
Pierre Demessence <pierre.demessence@gmail.com>
2019-06-10 16:51:16 +0200

    Reworked FindTarget script

--- Assets/Script/FindTarget.cs
+++ Assets/Script/FindTarget.cs
@@ -1,45 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class FindTarget : MonoBehaviour
 {
-    [SerializeField] private bool _canFindBetterTarget = true;
+    private readonly Collider2D[] _availableTargets = new Collider2D[10];
     [SerializeField] private float _homingRadius = 3;
+    [SerializeField] private bool _keepLookingForTargets = true;
     [SerializeField] private NewTargetAcquired _onNewTargetAcquired;
     private GameObject _target;
     [SerializeField] private LayerMask _targetLayer;
 
     private GameObject Target
     {
-        get => _target;
         set
         {
-            if (_target == value)
-                _onNewTargetAcquired.Invoke(value);
+            if (_target == value) return;
             _target = value;
+            _onNewTargetAcquired.Invoke(value);
+            if (!_keepLookingForTargets) enabled = false;
         }
+    }
+
+    private List<GameObject> FindTargets()
+    {
+        Array.Clear(_availableTargets, 0, 10);
+        Physics2D.OverlapCircleNonAlloc(transform.position, _homingRadius, _availableTargets, _targetLayer);
+        return _availableTargets.ToList().FindAll(c => c).ConvertAll(input => input.gameObject);
+    }
+
+    private GameObject FindBestTarget(IReadOnlyCollection<GameObject> targets)
+    {
+        if (targets.Count == 0) return null;
+        
+        var target = targets.First();
+        foreach (var availableTarget in targets.Skip(1))
+            if (Vector2.Distance(transform.position, availableTarget.transform.position) < Vector2.Distance(transform.position, target.transform.position))
+                target = availableTarget;
+
+        return target;
     }
 
     private void Update()
     {
-        if (!_canFindBetterTarget && _target) return;
-
-        var availableTargets = new Collider2D[10];
-        var colliders = Physics2D.OverlapCircleNonAlloc(transform.position, _homingRadius, availableTargets, _targetLayer);
-        if (colliders <= 0)
-        {
-            Target = null;
-            return;
-        }
-
-        var nonNullAvailableTargets = availableTargets.ToList().FindAll(c => c);
-        Target = nonNullAvailableTargets.First().gameObject;
-
-        foreach (var availableTarget in nonNullAvailableTargets)
-            if (Vector2.Distance(transform.position, availableTarget.transform.position) < Vector2.Distance(transform.position, Target.transform.position))
-                Target = availableTarget.gameObject;
+        Target = FindBestTarget(FindTargets());
     }
 }
 
